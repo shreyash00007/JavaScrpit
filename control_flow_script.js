00f703176b59cae26b36cfe925fa1d41a9a7efd3
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2023-11-08 19:50:42 +0530

    control flow

--- control_flow_script.js
+++ control_flow_script.js
@@ -1,2 +1,52 @@
 //------------------ Control flow JavaScript
 
+// Hour Greeting
+// If hour is between 6am and 12pm: Good morning!
+// If it is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evening!
+
+// ------------ If else
+
+// if (condition) {
+//     statement
+// }
+// else if (anotherCondition) {
+//     statement
+// }
+// else if (yetAnotherCondition) {
+//     statement
+// } else
+//     statement
+
+// Solution of hour greeting
+
+// let hour = 20;
+
+// if (hour >= 6 && hour < 12) {
+//     console.log('Good Morning');
+// } else if (hour >= 12 && hour < 18) {
+//     console.log("Good afternoon");
+// } else {
+//     console.log("Good evening");
+
+// ------------------- Switch Case statement
+
+let role;
+
+// switch (role) {
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+//     case 'moderator':
+//         console.log('Moderator User');
+//         break;
+//     default:
+//         console.log('Unknow User');
+// }
+
+// Simpler in if else
+
+// if (role === 'guest') console.log('Guest');
+// else if (role === 'moderator') console.log('Moderator');
+// else console.log('Unknown User');
+
