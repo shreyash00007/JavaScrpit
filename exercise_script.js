0be87a7c1686514add0821b4b572f17490ec0649
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2023-11-17 21:26:55 +0530

    isLandScape

--- exercise_script.js
+++ exercise_script.js
@@ -1,6 +1,7 @@
 // 1) Write a function that takes two numbers and returns
 // the maximum of the two.
 
+// My Solution ----
 function maximum(a, b) {
     if (a > b) {
         console.log(a , 'is greater number');
@@ -11,6 +12,36 @@ function maximum(a, b) {
     }
 }
 
-maximum(1, 'f');
+// maximum(1, 'f');
 
-// 2) 
+// M solution -----
+
+let number = max(3, 12);
+// console.log(number);
+
+function max(a, b) {
+    return (a > b) ? a : b;
+}
+
+// 2) Write a function which will tell if a image is
+// Landscape of not
+
+function isLandScape(width, height) {
+    if (width > height) {
+        console.log('is landscape');
+    } else if (width < height) {
+        console.log('is not LandScape');
+    } else {
+        console.log('Invalid Input');
+    }
+}
+// isLandScape(1, 12);
+
+// M Solution
+
+function isLandScaped(width, height) {
+    // return (width > height) ? true : false; // it not optimal 
+    return (width > height); // same as above it will retun a bollean value casue it is using compraison operators 
+}
+
+console.log(isLandScaped(800, 600));
