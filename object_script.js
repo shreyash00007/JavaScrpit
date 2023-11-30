fbc4d0d9e96d980a480c2763cd4e56264144a791
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2023-11-30 15:52:44 +0530

    factory & and constructor functions

--- object_script.js
+++ object_script.js
@@ -1,8 +1,8 @@
+// -------------------- Intorduction to Objects
 // Objects are collection of key value pairs
-
 // Object-Oriented Programming (OOP)
-
-const circle = {
+// Object litral syntax V
+const circleE = { 
     radius: 1,
     location: {
         x: 1,
@@ -14,4 +14,58 @@ const circle = {
     }
 };
 
-circle.draw();
+// circle.draw();
+
+// Two types of Functions
+// 1) Factory Function
+// 2) Constructor Function
+
+// ----------- Factory Function
+// Function and Method
+// A function doesn't need any object and is independent,
+// while the method is a function, which is linked with any object.
+
+// Factory functions are the functions which produced Object's
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('Factory function ')
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+// console.log(circle1, circle1.draw());
+
+const circle2 = createCircle(3);
+// console.log(circle2, circle2.draw())
+
+
+// ------------ Constructor Function
+// Camel Notation : oneTwoThreeFour
+// Pascal Notaton : OneTwoThreeFour
+
+// this is key word in JavaScript
+// which has reference to the object that is executing 
+// this piece of code
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1); // the new keyword creates a empty object
+// const x = {}; empty object
+
+// Difference between factory function and constructor fuction
+// In factory function create a object and pass argument
+// with factory function we simply call a function and return a new object
+
+// In constructor we use new operator and instead of returning and object
+// we use this. key word also in nameing convention we use pascal naming convention
+
+console.log(circle,circle.draw());
