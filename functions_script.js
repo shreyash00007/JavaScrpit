9a33ecf50ded34f769badb7c057266edc1c80805
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-01-09 17:20:17 +0530

    Hoisting in JavaScript

--- functions_script.js
+++ functions_script.js
@@ -1,6 +1,6 @@
 // ___________________________Functions in JavaScript____________________________
 
-// Function Declaration
+// ------------------ Function Declaration ---------------------
 
 function walk() {
     console.log('walk');
@@ -13,14 +13,45 @@ let run = function () {
     console.log('run');
 };
 
-run();
+// run();
 
 let move = run;
-move();
+// move();
 
 //  Named Function expression
 let play = function boy() {
     console.log('play');
 };
 
-play();
+// play();
+
+// Random
+
+// const obj = {
+//     value: 'hello',
+//     print() {
+//         setTimeout(function () {
+//             console.log(this.value);
+//         },0)
+//     }
+// }
+// obj.print();
+
+//----------------------- Hoisting --------------------------
+
+// consolSe.log(x); // will throw error due to x is not defined 
+let x = 1;
+
+// cooks(); // will throw a error cooks is not defined
+
+const cooks = function cook() {
+    console.log('cook');
+}
+
+cat(); // it will work
+// Hosting is a process of moving function declartion to top of the file 
+// which hapeens in automatically in javascript.
+
+function cat() {
+    console.log('cat');
+}
