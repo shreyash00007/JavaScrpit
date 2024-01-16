2dca87b576f71bd8bf5f5bc7e8c18f501e1c47ac
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-01-16 14:20:55 +0530

    oops

---
+++ Advanced_JavaScript/oop.js
@@ -0,0 +1,17 @@
+// ---------------------------- Object Oriented Programming --------------------------------
+// what is OOP?
+// A programming paradigm centered around objects rather than functtions.
+// It's a style of programming whichi center around objects.
+// Languages -- C#, Java, Ruby, Python, & JavaScript
+// Before oops we had procedural programming that divided a program into functions
+// but as the programs grows ups chose and create spagehti code inter dependenci
+
+
+// ------------ Four pillars of oops ------------------
+
+// Encapsulation
+// Abstracttion
+// Inheritance
+// Polymorphihsm
+
+
