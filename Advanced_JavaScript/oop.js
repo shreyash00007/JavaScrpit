4e7725fe36c0044e56540c4f2f99ddd5ee94c479
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-04-07 10:14:02 +0530

    op

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -63,13 +63,13 @@ function Circle(radius) {
     }
 }
 
-// const another = new Circle(1);
+const another = new Circle(1);
 
 
 // ---------- cosntructor Property -------------
 
 // let x = {};
-// console.log(x);
+console.log(x);
 
 new String(); // '',"",``
 new Boolean(); // true, false
