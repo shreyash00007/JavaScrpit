df53f0c11e93abfdd94d5bc174d8eaa55e73b364
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-04-11 23:22:23 +0530

    up

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -94,7 +94,7 @@ new Number(); // 1, 2, 3, ....
 // Circle.call({}, 1);
 // Circle.apply({}, 1, 2, 3);
 
-const another = new Circle(1);
+// const another = new Circle(1);
 // const circle = new Circle1(1);
 
 // console.log(another);
