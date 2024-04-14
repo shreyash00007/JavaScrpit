acb21a57aab799ebce8373f6bbe24fad7d67624b
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2024-04-14 11:17:56 +0530

    Update oop.js

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -2,7 +2,7 @@
 //What is OOP?
 // A programming paradigm centered around objects rather than functions.
 // It's a style of programming which centers around objects.
-// Languages -- C#, Java, Ruby, Python, & JavaScript
+// Languages -- C#, Java, Ruby, Python, & JavaScript are oops languages
 // Before oops we had procedural programming that divided a program into functions
 // but as the programs grow ups choose and create spaghetti code interdependencies
 
