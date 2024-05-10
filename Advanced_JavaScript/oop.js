0a100679e5741a589f484ce68abd6296150fa47a
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-05-10 22:23:56 +0530

    date in js

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -306,3 +306,9 @@ circle.propertyName = { x: 2 };
 // Shreyash`
 
 // console.log(another);
+
+// -------------- Date
+
+const now = new Date();
+const date1 = new Date('May 11 2018 9:00');
+const date2 = new Date(2018, 4, 11, 9);
