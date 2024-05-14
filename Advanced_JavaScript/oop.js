c63058a3a3aad5f73dbf6f06b029d1e4e37dd0f7
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-05-14 12:41:09 +0530

    date

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -312,3 +312,8 @@ circle.propertyName = { x: 2 };
 const now = new Date();
 const date1 = new Date('May 11 2018 9:00');
 const date2 = new Date(2018, 4, 11, 9);
+
+// now.setFullYear(2017);
+console.log(now.toTimeString());
+console.log(date1);
+console.log(date2);
