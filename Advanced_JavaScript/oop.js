7c2ad3bdcf1599534c77ba3cc92b0a4a852743da
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-05-16 12:15:45 +0530

    Update oop.js

--- Advanced_JavaScript/oop.js
+++ Advanced_JavaScript/oop.js
@@ -314,6 +314,7 @@ const date1 = new Date('May 11 2018 9:00');
 const date2 = new Date(2018, 4, 11, 9);
 
 // now.setFullYear(2017);
+// new year
 console.log(now.toTimeString());
 console.log(date1);
 console.log(date2);
