d5f46f0db98265c6b931adb9836a37039f252718
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-05-28 11:37:21 +0530

    startTime

--- Advanced_JavaScript/advance_exercise.js
+++ Advanced_JavaScript/advance_exercise.js
@@ -7,7 +7,12 @@
 function Stopwatch() {
     let startTime, endTime, running, duration = 0;
     this.start = function () {
-        
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+
+        startTime = new Date();
     };
     this.end = function () {
 
