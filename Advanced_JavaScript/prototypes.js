9b7124faf06a217a9756c3382cfd7a9166daf26f
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-06-01 19:08:36 +0530

    inheritance

--- Advanced_JavaScript/prototypes.js
+++ Advanced_JavaScript/prototypes.js
@@ -3,3 +3,4 @@
 // Inheritance of the core concept of opp that enables an object that takes on the properties and methods
 // of another object and this makes it esay to reuse code in different parts of an application
 
+// Inheritance--------------------
