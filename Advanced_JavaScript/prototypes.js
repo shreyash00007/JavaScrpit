4fb07e97618fb0780721b3af5733475e0b978e4c
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-06-03 22:39:59 +0530

    prototypes

--- Advanced_JavaScript/prototypes.js
+++ Advanced_JavaScript/prototypes.js
@@ -3,4 +3,8 @@
 // Inheritance of the core concept of opp that enables an object that takes on the properties and methods
 // of another object and this makes it esay to reuse code in different parts of an application
 
-// Inheritance--------------------
+// --------------------- Prototypes and Prototypical Inheritance --------------------
+
+let x = {};
+console.log(x);
+// Here x is a prototypical object it has some inheriant properities 
