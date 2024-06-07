b8efe88a79f0a3190721aa80036f073b72d11a3f
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-06-07 22:35:11 +0530

    Multilevel Inheritance

--- Advanced_JavaScript/prototypes.js
+++ Advanced_JavaScript/prototypes.js
@@ -2,13 +2,18 @@
 
 // Inheritance of the core concept of opp that enables an object that takes on the properties and methods
 // of another object and this makes it esay to reuse code in different parts of an application
-
+// in inheritance you can inherit fucntional properties in a object
+// it is mostly used in objects and classes
+// It also one the most important pillar of Object oriented programming
 // --------------------- Prototypes and Prototypical Inheritance --------------------
 
 let x = {};
 console.log(x);
-
 // Here x is a prototypical object it has some inheriant properities
-// in inheritance you can inherit fucntional properties in a object
-// it is mostly used in objects and classes
-// It also one the most important pillar of Object oriented programming
+
+
+// ------------------------- Multilevel Inheritance --------------------------------
+
+let myArray = [];
+
+console.log(myArray);
