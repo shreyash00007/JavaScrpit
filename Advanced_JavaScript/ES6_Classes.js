91b74b2e35ab1b9b6d4ad1fd561db80ce219e0c6
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-08-18 11:30:29 +0530

    es6

---
+++ Advanced_JavaScript/ES6_Classes.js
@@ -0,0 +1,29 @@
+// ES6 Classes
+// ITs the modern version of JavaScript
+
+//  --------------------- Classic way for cosntructor
+
+// function Circle(radius) {
+//     this.radius = radius;
+
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// }
+
+// const cir = new Circle();
+// console.log(cir);
+
+
+// ----------------- new way of writing it  
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    draw() {
+        console.log('draw');
+    }
+}
+const c = new Circle(1);
+console.log(c);
