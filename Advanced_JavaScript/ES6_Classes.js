68152e7442dc2764e2bcbdf069e4247989309484
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-08-22 18:46:45 +0530

    hoisting and static methods

--- Advanced_JavaScript/ES6_Classes.js
+++ Advanced_JavaScript/ES6_Classes.js
@@ -27,3 +27,45 @@ class Circle {
 }
 const c = new Circle(1);
 // console.log(c);
+
+// ------------------------ Hoisting --------------------
+
+
+// sayHello(); // the hoisted
+// // function declaration
+// function sayHello() { }
+
+// // function expression
+// const sayGoodbye = function () { };
+// const number = 1;
+// // Function expression is not hoisted
+
+// // Class Declartion
+// class Circle {
+// }
+// // class declaration is not been hoisted
+// // Class Expression
+// const Square = class { };
+
+// ---------------- Static Methods -----------------
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    // Instance Method
+
+    draw() {     
+    }
+    
+    // Static Method
+    static parse(str) {
+        const radius = JSON.parse(str).radius;
+        return new Circle(radius);
+    }
+}
+
+const circle = new Circle(1);
+Circle.parse();
+console.log(circle);
