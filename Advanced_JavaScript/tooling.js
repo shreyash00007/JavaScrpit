50645bbb21713100c097a463ac9232b21ac739df
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-09-03 21:16:24 +0530

    modules and tooling

---
+++ Advanced_JavaScript/tooling.js
@@ -0,0 +1,21 @@
+// --------------------------- Tooling ----------------------------
+
+// ------------ Modularity ------------
+
+// Benifits of Modularity
+
+// Maintainability
+
+// Resue
+
+// Abstract
+
+
+// Modules and it's types
+
+// AMD----browser
+// CommonJS--------Nodejs
+// UMD----------Browser, Nodejs
+// ES6 Modules
+
+// focus on only tow CommonJS and ES6 Modules
