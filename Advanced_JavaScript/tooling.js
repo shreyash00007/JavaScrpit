93fb53f0014ad4198f647b298c957838d69e6c02
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2024-09-06 11:23:33 +0530

    done

--- Advanced_JavaScript/tooling.js
+++ Advanced_JavaScript/tooling.js
@@ -33,7 +33,16 @@
 
 // ----------- ES6  Modules ------------
 
-import { Circle } from './circle.js';
+// import { Circle } from './circle.js';
 
-const c = new Circle(10);
-c.draw();
+// const c = new Circle(10);
+// c.draw();
+
+
+// ---------------- ES6 Tooling -----------------
+
+// Tooling is the nothing but tools for javascript browser based applications
+// example like bundlers i.e Webpack and Bable
+
+a bundler takes the multiple files and makes them into a single file
+
