d08141101f4c6b4ab6d8fd84d9e8905f045471bc
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2025-03-16 23:55:38 +0530

    Update exercise_script.js

--- exercise_script.js
+++ exercise_script.js
@@ -1,4 +1,4 @@
-// ---------------------------------------- JavaScript Exercise ------------------------------------------------
+// --------------------------------------------- JavaScript Exercise -----------------------------------------------------
 // 1) Write a function that takes two numbers and returns
 // the maximum of the two.
 
